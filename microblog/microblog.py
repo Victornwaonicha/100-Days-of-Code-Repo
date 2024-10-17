e2db6f4bc4a9c5cb03a16926c3b992cf12b0cd9b
Victor <victornwaonicha@gmail.com>
2024-10-17 00:59:38 -0400

    Micro-Blog

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,1 +1,9 @@
 from app import bot_1
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import bot_1, db
+from app.models import User, Post
+
+@bot_1.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
