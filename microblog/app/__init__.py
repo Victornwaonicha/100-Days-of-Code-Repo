ee5c03cc99098926012ad2b2fee624878b6607ba
Victor <victornwaonicha@gmail.com>
2024-10-20 03:42:35 -0400

    Log_In_View

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
+# Being an innstance variable of Flask makes bot_1 a member of the app package.
 bot_1 = Flask(__name__)
+
+# Flask-Login initialization
+login = LoginManager(bot_1)
+login.login_view = 'login'
 
 #  Flask configuration
 bot_1.config.from_object(Config)
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,19 @@
 from flask import render_template, flash, redirect, url_for
 from app import bot_1
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from urllib.parse import urlsplit
 
 # Home view function
 @bot_1.route('/')
 @bot_1.route('/index')
+@login_required
 def index():
     user = {'username': 'Mallo'}
     posts = [
@@ -26,9 +35,21 @@ def index():
 # Login view function
 @bot_1.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+# Logout view function
+@bot_1.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
