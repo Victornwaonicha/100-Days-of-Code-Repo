f3cce6be823aa829049c6a920630c16d8c7c65e6
Victor <victornwaonicha@gmail.com>
2025-02-28 23:44:31 -0800

    Let Go

---
+++ All Python Files/debug_odd_even.py
@@ -0,0 +1,34 @@
+# number = int(input("What number do you want to check? "))
+
+# if number % 2 == 0:
+#     print("This is even number.")
+# else:
+#     print("Odd number.")
+
+
+# year = input("What year do you want to check? ")
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
+
+
+
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+    
+
