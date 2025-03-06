e0fe46117cb50f54c7f0f5bcc7855f8c319ee472
Victor <victornwaonicha@gmail.com>
2025-03-06 02:41:59 -0800

    Higher Lower Game

---
+++ All Python Files/higher_lower_game/main.py
@@ -0,0 +1,104 @@
+# Higher Lower Game To-Do List:
+# 1. Import necessary modules
+import random
+from random import choice
+
+
+# 2. Set up data
+from game_data import data
+
+# 3. Display game intro and rules
+name = input("What is your name?: ")
+print(f"Welcome to the Higher Lower Game {name}!")
+print("Guess which has more followers: A or B?")
+
+
+# 4. Pick two random options (A and B) from data
+option_a = choice(data)
+option_b = choice(data)
+while option_a == option_b:
+    option_b = choice(data)
+
+
+
+# # 5. Show A and B to player with details (hide follower counts)
+select_1 = f"Compare A: {option_a['name']}, a {option_a['description']} from {option_a['country']}"
+select_2 = f"Compare B: {option_b['name']}, a {option_b['description']} from {option_b['country']}"
+
+
+print(select_1)
+print(select_2)
+
+
+# 6. Get player’s guess (A or B)
+answer = input("Which has more followers, A or B:").upper()
+
+
+
+
+# 7. Compare follower counts to check if guess is correct
+is_correct = False
+count_a = option_a['follower_count']
+count_b = option_b['follower_count']
+if count_a > count_b and answer == "A":
+    is_correct = True
+elif count_b > count_a and answer == "B":
+    is_correct = True
+else:
+    is_correct = False
+
+
+
+
+score = 0
+# 8. Give feedback (right/wrong) and update score
+if is_correct:
+    score = score +1
+    print(f"You are right! Current score: {score}")
+else:
+    print(f"Sorry, that's wrong.")
+
+
+
+# 9. Repeat with new B (A becomes old B if correct) until wrong guess
+while is_correct:
+    option_a = option_b
+    while option_a == option_b:
+        option_b = choice(data)
+    select_1 = f"Compare A: {option_a['name']}, a {option_a['description']} from {option_a['country']}"
+    select_2 = f"Compare B: {option_b['name']}, a {option_b['description']} from {option_b['country']}"
+
+
+    print(select_1)
+    print(select_2) 
+
+    answer = input("Which has more followers, A or B:").upper()
+
+    is_correct = False
+    count_a = option_a['follower_count']
+    count_b = option_b['follower_count']
+    if count_a > count_b and answer == "A":
+        is_correct = True
+    elif count_b > count_a and answer == "B":
+        is_correct = True
+    else:
+        is_correct = False
+
+
+    if is_correct:
+        score = score +1
+        print(f"You are right! Current score: {score}")
+    else:
+        print(f"Sorry, that's wrong.")
+
+
+
+# 10. End game with final score
+print(f"Game over, final score: {score}")
+
+
+
+
+
+
+
